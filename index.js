a7a30f1cf485b2350d2cf5d0f4459a810173c544
Emil Alexandrescu <emil.alexandresku@gmail.com>
2020-03-19 15:33:19 -0500

    configure chrome location

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const prerender = require('prerender');
-const server = prerender();
+const removeScriptTags = require('prerender/lib/plugins/removeScriptTags');
 const s3plugin = require('./s3-plugin');
 
-server.use(s3plugin);
+const options = {};
 
+if (process.env.CHROME_LOCATION) {
+  options.chromeLocation = process.env.CHROME_LOCATION;
+}
+
+server = prerender(options);
+
+server.use(removeScriptTags);
+server.use(s3plugin);
 server.start();
